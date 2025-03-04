e56220ca2e2f508c2599e8e73040a0ccb7fab8c6
Andrei Stepanenko <ftvkyo@gmail.com>
2025-03-04 20:48:43 +0000

    contour confines a point

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,15 @@
+use std::path::PathBuf;
+
+use clap::Parser;
+
+pub mod movement;
+pub mod types;
+
+#[derive(Parser)]
+pub struct Args {
+    pub input: PathBuf,
+}
+
 fn main() {
     println!("Hello, world!");
 }
---
+++ src/types.rs
@@ -0,0 +1,6 @@
+use nalgebra as na;
+
+pub type Float = f32;
+
+pub type Vector = na::Vector2<Float>;
+pub type Point = na::Point2<Float>;
